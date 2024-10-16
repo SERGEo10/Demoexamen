17992d7ba33c8e8d851ae2e80cf64b01ebbeb159
SERGEo <106819250+SERGEo10@users.noreply.github.com>
2024-10-16 13:32:48 +0300

    Delete Pole/modules directory

--- Pole/modules/manager.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import (    
-    QDialog # это базовый класс диалогового окна
-)
-
-class manager(QDialog):
-    def __init__(self):        
-        super(manager, self).__init__()
-        print("manager")
-
